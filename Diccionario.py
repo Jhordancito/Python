12c67cfc447ac2d18696b16add7e0ff9ed3dbb66
jhordan <gatojhordan@gmail.com>
2020-07-13 13:46:19 -0400

    Diccionarios

--- Diccionario.py
+++ Diccionario.py
@@ -7,4 +7,11 @@
 #diccionario = {"Alejandro":[22,1.73],"jose":[21,1.75],"maria":{"edad":22,"Estatura":1.87}}
 
 equipo = {10:"Paulo Dybala",11:"Douglas costa",7:"Cristiano Ronaldo"}
-print(equipo[7])
+#print(equipo[7])
+#print(equipo.get(19,"No existe un jugador dorsal")) #Mostrar mensaje que no existe
+#print(10 in equipo) #Modo Booleano
+#print(equipo.keys()) #Muestra las claves del diccionario
+#print(equipo.values()) #muestra los valores de cada diccionario
+#print(equipo.items()) #muestra los diccionarios separados
+#equipo.clear() Elimina los diccioanrios dentro de equipo
+print(len(equipo)) #muestra la cantidad de diccionarios
